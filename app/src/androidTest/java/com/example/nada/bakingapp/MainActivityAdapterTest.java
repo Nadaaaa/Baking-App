250eb2d0412bc4b282a0578e5bedab0f9ff8ec1c
Nada <nadabadr95@gmail.com>
2019-01-13 17:22:42 +0200

    Fix Test Case

--- app/src/androidTest/java/com/example/nada/bakingapp/MainActivityAdapterTest.java
+++ app/src/androidTest/java/com/example/nada/bakingapp/MainActivityAdapterTest.java
@@ -1,28 +1,68 @@
 package com.example.nada.bakingapp;
 
 
+import android.support.test.espresso.IdlingRegistry;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.example.nada.bakingapp.Activities.MainActivity;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityAdapterTest {
 
-    @Rule
+    /*@Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void adapterScroll() {
         onView(withId(R.id.rv_recipes)).perform(scrollToPosition(3));
+    }*/
+
+    private static final String RECIPE_ITEM = "Yellow Cake";
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    private IdlingResource mIdlingResource;
+
+    @Before
+    public void registerIdlingResource() {
+        mIdlingResource = activityTestRule.getActivity().getIdlingResource();
+        IdlingRegistry.getInstance().register(mIdlingResource);
+    }
+
+    @Test
+    public void adapterScroll() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(ViewMatchers.withId(R.id.rv_recipes)).perform(RecyclerViewActions.scrollToPosition(2));
+        onView(withText(RECIPE_ITEM)).check(matches(isDisplayed()));
+    }
+
+
+    @After
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            IdlingRegistry.getInstance().unregister(mIdlingResource);
+        }
     }
 }
--- app/src/main/java/com/example/nada/bakingapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/nada/bakingapp/Activities/MainActivity.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,6 +22,7 @@ import com.example.nada.bakingapp.Adapters.RecipesAdapter;
 import com.example.nada.bakingapp.Models.Recipe;
 import com.example.nada.bakingapp.NetwokUtils.Retrofit;
 import com.example.nada.bakingapp.R;
+import com.example.nada.bakingapp.Utils.SimpleIdlingResource;
 import com.example.nada.bakingapp.Utils.Utils;
 import com.example.nada.bakingapp.Widgets.WidgetUpdateService;
 import com.google.gson.Gson;
@@ -53,6 +58,18 @@ public class MainActivity extends AppCompatActivity implements RecipesAdapter.Li
     //Vars
     private RecipesAdapter mRecipesAdapter;
     private static List<Recipe> tempRecipesList;
+
+    @Nullable
+    private SimpleIdlingResource mIdlingResource;
+
+    @VisibleForTesting
+    @NonNull
+    public IdlingResource getIdlingResource() {
+        if (mIdlingResource == null) {
+            mIdlingResource = new SimpleIdlingResource();
+        }
+        return mIdlingResource;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
